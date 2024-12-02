be01d0b8cd5641a6db6651c35f545bed948feca7
Ben Lovell <ben.j.lovell@gmail.com>
2024-12-02 23:14:04 +0100

    Python day1

--- day-1/day1.py
+++ day-1/day1.py
@@ -19,4 +19,14 @@ for i in range(len(max(left, right))):
 
 print(f"part one sum of differences is: {sum}")
 
-#
+# Part two
+
+similarity_score = 0
+for l in left:
+    similar_counter = 0
+    for r in right:
+        if l == r:
+            similar_counter += l
+    similarity_score += similar_counter
+
+print(f"similarity score is: {similarity_score}")
